f2d7c4b19d72eeb143b8d0de45ec5033cd95137f
Emmanuel Gonzalez <emm.gs17@gmail.com>
2025-06-26 17:59:54 -0600

    fix: delete unused rows in the hero section

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -4,7 +4,7 @@
 
 <section id="hero" class="flex justify-center mt-[8rem]">
   <div
-    class="grid grid-cols-1 sm:grid-rows-4 sm:grid-cols-2 grid-rows-8 w-2/3 gap-y-8 md:gap-x-8 xl:gap-x-32 2xl:gap-x-64"
+    class="grid flex-col sm:grid sm:grid-rows-4 sm:grid-cols-2 grid-rows-4 w-2/3 gap-y-8 md:gap-x-8 xl:gap-x-32 2xl:gap-x-64"
   >
     <h1
       class="font-tusker font-bold text-main-secondary text-[36px] xs:text-[42px] sm:text-3xl lg:text-[3rem] xl:text-[3.25rem] 2xl:text-[4rem] text-center sm:text-right sm:col-start-1 row-span-4 sm:row-span-2"
@@ -18,7 +18,7 @@
     </div>
     <div class="sm:row-start-3 flex justify-center sm:justify-end">
       <h2
-        class="inline-block w-3/4 text-main-neutral sm:text-xl lg:text-2xl 2xl:text-[2rem] font-extrabold text-center sm:text-left"
+        class="inline-block w-3/4 text-main-neutral text-lg lg:text-2xl 2xl:text-[2rem] font-extrabold text-center sm:text-left"
       >
         Some text about what the company is about
       </h2>
