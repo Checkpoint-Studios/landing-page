616bcc626287b621dacafbc7b696cd89889846a2
Emmanuel Gonzalez <emm.gs17@gmail.com>
2025-06-25 19:17:48 -0600

    feat: add the cta button

---
+++ src/lib/CtaButton.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { CircleArrowRight } from "@lucide/svelte"; 
+  let {
+    href = "/",
+    target = "_self",
+    color = "primary",
+    height = "sm",
+  }: {
+    href?: string;
+    target?: string;
+    height?: "sm" | "md";
+    color?: "primary" | "secondary";
+  } = $props();
+
+  let textColor = $derived(
+    color === "primary" ? "text-main-primary hover:text-primary-600" : "text-main-secondary hover:text-secondary-600",
+  );
+  let borderColor = $derived(
+    color === "primary" ? "border-main-primary hover:border-primary-600" : "border-main-secondary hover:border-secondary-600",
+  );
+  let heightClass = $derived(
+    height === "sm" ? "h-12" : "h-16",
+  );
+
+  let _class = $derived(
+    `flex items-center justify-between w-61 px-5 py-3 rounded-full border-4 font-tusker font-semibold text-[1.5rem] ${textColor} ${borderColor} ${heightClass} transition-all duration-300`,
+  );
+</script>
+
+<a {href} class={_class} {target}>
+  <p class="mt-1">Contact Us!</p>
+  <CircleArrowRight size="2rem" color="currentColor" />
+</a>
--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-  import logo from '../assets/images/logo.webp';
+  import logo from "../assets/images/logo.webp";
+  import CtaButton from "./CtaButton.svelte";
 </script>
 
 <nav class="flex justify-center items-start">
-  <div class="flex bg-main-neutral py-4 px-6 w-full h-21 rounded-b-[40px] sm:min-w-[600px] sm:w-1/2">
+  <div
+    class="flex justify-between bg-main-neutral py-4 px-6 w-full h-21 rounded-b-[40px] sm:min-w-[600px] sm:w-1/2"
+  >
     <a href="/" class="flex items-center">
-      <img src={logo} alt="" class="w-48 sm:w-auto sm:max-w-64 object-contain" /> 
+      <img
+        src={logo}
+        alt=""
+        class="w-48 sm:w-auto sm:max-w-64 object-contain"
+      />
     </a>
+    <div class="hidden md:flex">
+      <CtaButton />
+    </div>
   </div>
 </nav>
