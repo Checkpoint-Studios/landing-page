c8407be2175d8f4704914d66befc3b9f37ae349c
Emmanuel Gonzalez <emm.gs17@gmail.com>
2025-06-27 01:17:43 -0600

    fix: make the user card images circular

--- src/lib/ui/UserCard.svelte
+++ src/lib/ui/UserCard.svelte
@@ -13,7 +13,7 @@
 
 <div class="flex flex-col p-4 gap-8 justify-center items-center">
   <h3 class="text-main-neutral text-center">{name}</h3>
-  <img src={img} class="rounded-full w-[160px] 2xl:w-[320px] h-[160px] 2xl:h-full" alt="" />
+  <img src={img} class="rounded-full w-[160px] 2xl:w-[320px] h-[160px] 2xl:h-[320px]" alt="" />
   <h5 class="text-main-neutral text-center">{role}</h5>
 </div>
 
