172e97bbd784810259879de9e04781d198b3ff5c
Emmanuel Gonzalez <emm.gs17@gmail.com>
2025-06-27 21:15:07 -0600

    fix: change the navbar links

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -16,7 +16,7 @@
   <div
     class="flex justify-center md:justify-between bg-main-neutral py-4 px-6 w-full h-21 rounded-b-[40px] sm:min-w-[600px] sm:w-1/2"
   >
-    <a href="/" class="flex items-center" aria-label="Home">
+    <div class="flex items-center">
       {#await preload(logo) then logo}
       <img
         src={logo}
@@ -24,9 +24,9 @@
         class="w-48 sm:w-auto sm:max-w-64 object-contain"
       />
       {/await}
-    </a>
+    </div>
     <div class="hidden md:flex">
-      <CtaButton />
+      <CtaButton href="#contact" />
     </div>
   </div>
 </nav>
